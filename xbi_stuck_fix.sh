7f60d5cdf7aea54e641fca95d0f832dc5ba77cbc
nashsclay <nashsclay@gmail.com>
2019-03-20 11:27:17 -0700

    Update xbi_stuck_fix.sh

--- xbi_stuck_fix.sh
+++ xbi_stuck_fix.sh
@@ -10,6 +10,7 @@ sudo apt-get install unrar
 echo "Script may appear frozen for a few seconds. This is normal."
 wget -c https://github.com/sub307/XBI-bootstrap/releases/download/417018/bootstrap417018.rar
 sudo unrar x bootstrap417018.rar fix/
+cd fix
 sudo rm -R ~/.XBI/backups/
 sudo rm -R ~/.XBI/blocks/
 sudo rm -R ~/.XBI/chainstate/
@@ -17,7 +18,9 @@ sudo rm -R ~/.XBI/database/
 sudo rm -R ~/.XBI/sporks/
 sudo rm -R ~/.XBI/zerocoin/
 sudo rm ~/.XBI/budget.dat ~/.XBI/db.log ~/.XBI/debug.log ~/.XBI/fee_estimates.dat ~/.XBI/mncache.dat ~/.XBI/mnpayments.dat ~/.XBI/peers.dat ~/.XBI/xbid.dat ~/.XBI/.lock
-sudo rm bootstrap417018.rar
+sudo rm ../bootstrap417018.rar
 sudo mv chainstate blocks peers.dat ~/.XBI
+cd /usr/local/bin/
 systemctl start XBI.service
+cd ~
 echo "Masternode restarted. Any errors about files not existing is ok."
